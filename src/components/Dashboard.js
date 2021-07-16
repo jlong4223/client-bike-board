3939595e916a08e422665014b74e774894384537
Jared Long <jlong4223@gmail.com>
2021-07-16 17:03:01 -0400

    chore: initial component setup/boilerplate

---
+++ src/components/Dashboard.js
@@ -0,0 +1,9 @@
+const Dashnoard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashnoard;
---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h1>The page you are looking for has not been found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
