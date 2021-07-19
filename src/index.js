4a2817bc9c3545058113be0403770fe3c6cae622
Jared Long <jlong4223@gmail.com>
2021-07-19 11:00:04 -0400

    chore: updated store to use redux devtools package

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import { Router, Route } from "react-router";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import App from "./App";
 import history from "./utils/history";
 import reducers from "./redux/reducers";
-import composeEnhancers from "./utils/reduxDevTools";
 
 const store = createStore(
   reducers,
-  composeEnhancers(applyMiddleware(reduxThunk))
+  composeWithDevTools(applyMiddleware(reduxThunk))
 );
 
 ReactDOM.render(
