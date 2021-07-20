ecdddbaa2321e43674f9294146c88136beed18cd
Jared Long <jlong4223@gmail.com>
2021-07-20 16:06:31 -0400

    feat: sets up token functions w/local storage

---
+++ src/services/token-service.js
@@ -0,0 +1,28 @@
+function setToken(token) {
+  token
+    ? localStorage.setItem("rails_token", token)
+    : localStorage.removeItem("rails_token");
+}
+
+function removeToken() {
+  localStorage.removeItem("token");
+}
+
+function getToken(res) {
+  let token = localStorage.getItem("rails_token");
+  if (token) {
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    if (payload.exp < Date.now() / 1000) {
+      localStorage.removeItem("rails_token");
+      token = null;
+    }
+  }
+  return token;
+}
+
+function getEntireUserFromToken() {
+  const token = getToken();
+  return token ? JSON.parse(atob(token.split(".")[1])) : null;
+}
+
+export { setToken, getToken, removeToken, getEntireUserFromToken };
