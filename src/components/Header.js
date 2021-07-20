e038e006ae84993a34b03caf31add4322ff31f78
Jared Long <jlong4223@gmail.com>
2021-07-20 16:09:33 -0400

    feat: connects comp w/props and logout action

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,39 @@
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "../redux/actions";
 
-const Header = () => {
+const Header = ({ userInfo, logout }) => {
   const styles = headerStyles();
+
+  const isSignedIn = userInfo.isSignedIn;
+
   return (
     <header style={styles.header}>
       <h1>Client BikeBoard</h1>
-      <div style={styles.authLinks}>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Signup</Link>
-      </div>
+      {isSignedIn ? (
+        <div style={styles.authLinks}>
+          <p>Hi, {userInfo.user.name}</p>
+          <Link to="" onClick={() => logout()}>
+            Logout
+          </Link>
+        </div>
+      ) : (
+        <div style={styles.authLinks}>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </div>
+      )}
     </header>
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    userInfo: state.userInfo,
+  };
+};
+
+export default connect(mapStateToProps, { logout })(Header);
 
 const headerStyles = () => {
   return {
