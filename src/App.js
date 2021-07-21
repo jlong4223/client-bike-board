adb0a92ad492073cf1ae9bc3d6f8d85b24a3e0a2
Jared Long <jlong4223@gmail.com>
2021-07-21 16:35:38 -0400

    feat: shows header on all routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { Switch, Route } from "react-router-dom";
 
 import routes from "./routes";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Switch>
-      {routes.map((route) => (
-        <Route
-          key={route.id}
-          path={route.path}
-          exact={route.exact}
-          component={route.component}
-        />
-      ))}
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        {routes.map((route) => (
+          <Route
+            key={route.id}
+            path={route.path}
+            exact={route.exact}
+            component={route.component}
+          />
+        ))}
+      </Switch>
+    </>
   );
 }
 
