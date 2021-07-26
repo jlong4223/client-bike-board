10123f0fdafb16984f7a2f1bf1e2c1fb71753549
Jared Long <jlong4223@gmail.com>
2021-07-26 13:06:10 -0400

    refactor: moved fields to seperate file

---
+++ src/components/profile/ProfileFields.js
@@ -0,0 +1,44 @@
+export const fields = [
+  {
+    name: "address",
+    type: "text",
+    label: "Enter your Address",
+    placeholder: "Enter your address",
+    required: true,
+  },
+  {
+    name: "bio",
+    type: "textarea",
+    label: "Enter your Bio",
+    placeholder: "Enter your bio",
+    required: true,
+  },
+  {
+    name: "biker_type",
+    type: "text",
+    label: "What kind of biker are you?",
+    placeholder: "What kind of biker are you?",
+    required: true,
+  },
+  {
+    name: "bike_count",
+    type: "text",
+    label: "How many bikes do you own?",
+    placeholder: "How many bikes do you own?",
+    required: true,
+  },
+  {
+    name: "phone",
+    type: "text",
+    label: "Enter your phone number",
+    placeholder: "Enter your phone number",
+    required: true,
+  },
+  {
+    name: "current_bike",
+    type: "text",
+    label: "Enter your current bike",
+    placeholder: "Enter your current bike",
+    required: true,
+  },
+];
