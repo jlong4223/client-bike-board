90c7e330cfe793dc3d056f98ace5dd00bc959c57
Jared Long <jlong4223@gmail.com>
2021-07-28 11:14:37 -0400

    feat: set up service functions for user & details with rails api

--- src/services/user-details-rails.js
+++ src/services/user-details-rails.js
@@ -1,5 +1,14 @@
 import railsApi from "./rails-url";
 
-export const getDetails = async (userId) => {
+export const getDetailsByUser = async (userId) => {
   return await railsApi.get(`/users/${userId}`);
+};
+
+export const getDetailsById = async (detailsId) => {
+  return await railsApi.get(`/details/${detailsId}`);
+};
+
+// TODO to post details, just post to /details, include the formdata and the userId - untested
+export const postDetails = async (userId, formValues) => {
+  return await railsApi.post("/details");
 };
