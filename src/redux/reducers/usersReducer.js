63bc7398f2458279c93a09b6786a9e6ff5f6a22e
Jared Long <jlong4223@gmail.com>
2021-07-30 21:55:32 -0500

    feat: adds new case for pics to usersReducer

--- src/redux/reducers/usersReducer.js
+++ src/redux/reducers/usersReducer.js
@@ -1,9 +1,16 @@
-import { REGISTER, LOGIN, LOGOUT, UPDATE_USER_DETAILS } from "../actions/types";
+import {
+  REGISTER,
+  LOGIN,
+  LOGOUT,
+  UPDATE_USER_DETAILS,
+  UPDATE_PIC,
+} from "../actions/types";
 
 const initialState = {
   user: {},
   details: {},
   isSignedIn: false,
+  pics: {},
 };
 
 export default function usersReducer(state = initialState, action) {
@@ -14,6 +21,8 @@ export default function usersReducer(state = initialState, action) {
       return { ...state, isSignedIn: true, user: action.payload };
     case UPDATE_USER_DETAILS:
       return { ...state, details: action.payload };
+    case UPDATE_PIC:
+      return { ...state, pics: action.payload };
     case LOGOUT:
       return { ...state, isSignedIn: false, user: {}, details: {} };
     default:
