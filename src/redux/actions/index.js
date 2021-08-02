378e12033f9186986de6013ef288eaa697466731
Jared Long <jlong4223@gmail.com>
2021-08-02 14:25:30 -0500

    feat: exporting adidtional action for pic

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,4 +1,13 @@
 import { registerUser, getUserFromDB, logout } from "./auth";
 import { editUserDetails, getDetails } from "./details";
+import { updatePic, getUsersPics } from "./profilePic";
 
-export { registerUser, getUserFromDB, logout, editUserDetails, getDetails };
+export {
+  registerUser,
+  getUserFromDB,
+  logout,
+  editUserDetails,
+  getDetails,
+  updatePic,
+  getUsersPics,
+};
