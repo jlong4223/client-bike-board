81d97d0893960cfa13e5f1f62b97db1f00b49050
Jared Long <jlong4223@gmail.com>
2021-08-09 21:53:04 -0500

    refactor: updates action imports and funcs

--- src/redux/actions/profilePic.js
+++ src/redux/actions/profilePic.js
@@ -1,19 +1,15 @@
-import cloudinaryAPI from "../../services/node-cloud-url";
+import { getUserPic, postNewPic } from "../../services/user-details";
 import { UPDATE_PIC } from "./types";
 
 export const updatePic = (pic) => {
   console.log("update pic", pic);
 
   return async (dispatch) => {
-    const response = await cloudinaryAPI.post("/allapps", pic);
+    const response = await postNewPic(pic);
     console.log("response in update pic", response);
 
-    const getUserFromNode = await cloudinaryAPI.get(
-      `/allapps/client-bikeboard/${response.data.user[0].userID}`
-    );
+    const getUserFromNode = await getUserPic(response.data.user[0].userID);
     console.log("getUserFromNode", getUserFromNode);
-
-    // The response is faster than cloudinary uploads images so need to figure out how to wait for the image to upload before updating the user- maybe this is a backend thing
 
     dispatch({
       type: UPDATE_PIC,
@@ -24,10 +20,9 @@ export const updatePic = (pic) => {
 
 export const getUsersPics = (userID) => {
   return async (dispatch) => {
-    const response = await cloudinaryAPI.get(
-      `/allapps/client-bikeboard/${userID}`
-    );
+    const response = await getUserPic(userID);
     console.log("response in get pics", response.data[0].picture);
+
     dispatch({
       type: UPDATE_PIC,
       payload: response.data[0].picture,
