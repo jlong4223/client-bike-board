14423ae03afde32c0869af86c13ccad5c0e539ed
Jared Long <jlong4223@gmail.com>
2021-08-09 21:53:29 -0500

    refactor: creates pic funcs in user services

---
+++ src/services/user-details.js
@@ -0,0 +1,23 @@
+import railsApi from "./rails-url";
+import cloudinaryApi from "./node-cloud-url";
+
+export const getDetailsByUser = async (userId) => {
+  return await railsApi.get(`/users/${userId}`);
+};
+
+export const getDetailsById = async (detailsId) => {
+  return await railsApi.get(`/details/${detailsId}`);
+};
+
+// TODO to post details, just post to /details, include the formdata and the userId - untested & not used yet
+export const postDetails = async (userId, formValues) => {
+  return await railsApi.post("/details");
+};
+
+export const getUserPic = async (userId) => {
+  return await cloudinaryApi.get(`/allapps/client-bikeboard/${userId}`);
+};
+
+export const postNewPic = async (pic) => {
+  return await cloudinaryApi.post("/allapps/", pic);
+};
