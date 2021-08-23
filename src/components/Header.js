7d5554dcb7af0ea84a33ae4b0a79905e9b0921bc
Jared Long <jlong4223@gmail.com>
2021-08-22 22:02:25 -0500

    fix: gets imgs & profilePic on signin for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { logout } from "../redux/actions";
+import { logout, getUsersPics } from "../redux/actions";
 
-const Header = ({ userInfo, logout, profilePic }) => {
-  const styles = headerStyles();
-
+const Header = ({ userInfo, logout, profilePic, getUsersPics }) => {
   const isSignedIn = userInfo.isSignedIn;
+
+  useEffect(() => {
+    isSignedIn && getUsersPics(userInfo.user.user_id);
+  }, [isSignedIn, getUsersPics, userInfo.user.user_id]);
+
+  const styles = headerStyles();
 
   return (
     <header style={styles.header}>
@@ -23,7 +28,7 @@ const Header = ({ userInfo, logout, profilePic }) => {
           <Link to={`/profile/${userInfo.user.name}`} style={styles.normalLink}>
             Hi, {userInfo.user.name}
           </Link>
-          {/* TODO idea- onclick brings up the images, and it first sets the image clicked to false and then changes the image that the user selects as an image */}
+          {/* TODO idea- onclick brings up the images only if the user has images, and it first sets the image clicked to false and then changes the image that the user selects as an image */}
           <img style={styles.image} src={profilePic} alt="profileImg" />
         </div>
       ) : (
@@ -48,7 +53,7 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, { logout })(Header);
+export default connect(mapStateToProps, { logout, getUsersPics })(Header);
 
 const headerStyles = () => {
   return {
